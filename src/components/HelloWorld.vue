be797e89e6c0b5815f764608abd8adfd9ac9423e
wanglin2 <1013335014@qq.com>
2022-10-25 10:44:30 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
